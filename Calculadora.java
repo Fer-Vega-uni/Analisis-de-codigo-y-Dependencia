5c8a48cb65f5222efc933d8b0cf8df280181c597
pepi <f.vega11@gmail.com>
2025-10-17 00:32:09 -0300

    Primer commit: código base dado por Visual Paradigm.

---
+++ Calculadora.java
@@ -0,0 +1,40 @@
+public class Calculadora {
+
+	private int n1;
+	private int n2;
+
+	public void setN1(int n1) {
+		this.n1 = n1;
+	}
+
+	public void setN2(int n2) {
+		this.n2 = n2;
+	}
+
+	public Calculadora() {
+		// TODO - implement Calculadora.Calculadora
+
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param n1
+	 * @param n2
+	 */
+	public Calculadora(int n1, int n2) {
+		// TODO - implement Calculadora.Calculadora
+		throw new UnsupportedOperationException();
+	}
+
+	public int sumar() {
+		// TODO - implement Calculadora.sumar
+		throw new UnsupportedOperationException();
+	}
+
+	public int multiplicar() {
+		// TODO - implement Calculadora.multiplicar
+		throw new UnsupportedOperationException();
+	}
+
+}
